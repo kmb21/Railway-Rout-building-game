04d31790517f8303ebd912771421e7f93c04f53a
Maxwell B Kumbong <113995857+kmb21@users.noreply.github.com>
2023-07-18 00:52:44 -0400

    Update stlHashTable.h

--- adts/stlHashTable.h
+++ adts/stlHashTable.h
@@ -1,10 +1,4 @@
-/*
-  Copyright (c) 2023
-  Swarthmore College Computer Science Department, Swarthmore PA
-  J. Brody, A. Danner, L. Fontes, L. Meeden, Z. Palmer, A. Soni
-  Distributed as course material for Spring 2023
-  CPSC 035: Data Structures and Algorithms
-*/
+
 
 #pragma once
 #include <stdexcept>
