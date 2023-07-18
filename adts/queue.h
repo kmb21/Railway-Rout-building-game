2485cd9c42b425bfe2c557fea05f48a8823de77c
Maxwell B Kumbong <113995857+kmb21@users.noreply.github.com>
2023-07-18 00:51:28 -0400

    Update queue.h

--- adts/queue.h
+++ adts/queue.h
@@ -1,13 +1,5 @@
 #pragma once
 
-/*
-  Copyright (c) 2023
-  Swarthmore College Computer Science Department, Swarthmore PA
-  J. Brody, A. Danner, L. Fontes, L. Meeden, Z. Palmer, A. Soni
-  Distributed as course material for Spring 2023
-  CPSC 035: Data Structures and Algorithms
-*/
-
 #include "orderedCollection.h"
 
 /**
